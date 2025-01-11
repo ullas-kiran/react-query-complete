b4136b2482420b4638133cbd105e2c8eb3b2a86f
ullas-kiran <ullas261@gmail.com>
2025-01-11 20:11:18 +0530

    query import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
-const queryClient=new QueryClient;
+const queryClient = new QueryClient;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
