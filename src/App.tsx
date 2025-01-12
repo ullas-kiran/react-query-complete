41192438f15394e6154e03af1cc04491f8a674cf
ullas-kiran <ullas261@gmail.com>
2025-01-12 20:55:39 +0530

    useMutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import { useQuery } from '@tanstack/react-query'
+import "./App.css";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-  <div>
-
-  </div>
-  )
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
-export default App
+function App() {
+  const { data, isLoading, error } = useQuery<Todo[]>({
+    queryKey: ["todo"],
+    queryFn: () =>
+      fetch("https://jsonplaceholder.typicode.com/todos").then((res) => (res.json()))
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error instanceof Error) return <div>Error: {error.message}</div>;
+
+const {mutate,isPending,isError,isSuccess} =useMutation({mutationFn:(newPost)=> fetch("https://jsonplaceholder.typicode.com/todos",{
+  method:"POST",
+  body:JSON.stringify(newPost)
+}).then((res) => (res.json()))})
+
+  return (
+    <div>
+      {isPending&&<p>Data is being added...</p>}
+      {data?.map((todo: Todo) => (
+        <div key={todo.id}>
+          <h1>Id: {todo.id}</h1>
+          <p>Title: {todo.title}</p>
+          <p>Completed: {todo.completed ? "Yes" : "No"}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
