4e391286d5e37b39f1c216d7567d56517932fee8
ullas-kiran <ullas261@gmail.com>
2025-01-16 21:45:03 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,41 @@ interface Todo {
   completed: boolean;
 }
 
+type PostData = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
 function App() {
+  // Fetch data using useQuery
   const { data, isLoading, error } = useQuery<Todo[]>({
     queryKey: ["todo"],
     queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/todos").then((res) => (res.json()))
+      fetch("https://jsonplaceholder.typicode.com/todos").then((res) => res.json())
   });
 
+  // Error and Loading states
   if (isLoading) return <div>Loading...</div>;
   if (error instanceof Error) return <div>{error.message}</div>;
 
-  // mutation for post 
-const {mutate,isPending,isError,isSuccess} =useMutation({mutationFn:(newPost)=> fetch("https://jsonplaceholder.typicode.com/todos",{
-  method:"POST",
-  body:JSON.stringify(newPost)
-}).then((res) => (res.json()))})
+  // Mutation for creating a new todo item
+  // const { mutate, isPending, isError, isSuccess } = useMutation({
+  //   mutationFn: (newPost: PostData) =>
+  //     fetch("https://jsonplaceholder.typicode.com/todos", {
+  //       method: "POST",
+  //       body: JSON.stringify(newPost),
+  //     }).then((res) => res.json()),
+  // });
 
   return (
     <div>
-      {isPending&&<p>Data is being added...</p>}
-      <button onClick={()=>
-        mutate({
-          userId:5000,
-          id:4000,
-          title:"hey lets do this"
-        })
-      }>
-        Add Post
-      </button>
+
+      {/* Uncomment this button to trigger the mutation */}
+      
+     
+
+      {/* Render fetched todos */}
       {data?.map((todo: Todo) => (
         <div key={todo.id}>
           <h1>Id: {todo.id}</h1>
