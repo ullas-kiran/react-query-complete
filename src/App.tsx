80b13af57022d2beec7e591c8be68aab9293ca4f
ullas-kiran <ullas261@gmail.com>
2025-01-22 19:43:14 +0530

    remove unwanted code

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,23 @@ interface Todo {
   completed: boolean;
 }
 
-type PostData = {
-  userId: number;
-  id: number;
-  title: string;
-};
+
 
 function App() {
-  // Fetch data using useQuery
   const { data, isLoading, error } = useQuery<Todo[]>({
     queryKey: ["todo"],
     queryFn: () =>
       fetch("https://jsonplaceholder.typicode.com/todos").then((res) => res.json())
   });
 
-  // Error and Loading states
+
   if (isLoading) return <div>Loading...</div>;
   if (error instanceof Error) return <div>{error.message}</div>;
 
-  // const mutation=useMutation({mutationFn})/
+
 
   return (
     <div>
-
-      {/* Uncomment this button to trigger the mutation */}
-      
-     
-
-      {/* Render fetched todos */}
       {data?.map((todo: Todo) => (
         <div key={todo.id}>
           <h1>Id: {todo.id}</h1>
