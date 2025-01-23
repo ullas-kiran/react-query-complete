39b328d21a3f045d2305f47611921d986c12d8f4
ullas-kiran <ullas261@gmail.com>
2025-01-23 21:52:01 +0530

    bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,74 @@
+import { getUsers } from "./utils/api";
 import "./App.css";
 import { useMutation, useQuery } from "@tanstack/react-query";
-
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
+import { UserResponseHttpData } from "./utils/types";
 
 
 
 function App() {
-  const { data, isLoading, error } = useQuery<Todo[]>({
-    queryKey: ["todo"],
-    queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/todos").then((res) => res.json())
-  });
+  const { data, isLoading, error } = useQuery<UserResponseHttpData[]>({
+    queryKey: ["getUsers"],
+    queryFn: getUsers
+   })
 
 
   if (isLoading) return <div>Loading...</div>;
   if (error instanceof Error) return <div>{error.message}</div>;
 
+  const {
+		mutate: createPostMutation,
+		isSuccess: isCreatePostSuccess,
+		isPending: isCreatePostPending,
+	} = useMutation({
+		mutationFn: createPost,
+		onSuccess: () => {
+			console.log("onSuccess");
+			queryClient.invalidateQueries({ queryKey: ["getPosts"] });
+			queryClient.invalidateQueries({ queryKey: ["getUsers"] });
+		},
+	});
 
 
   return (
     <div>
-      {data?.map((todo: Todo) => (
+      {/* {data?.map((todo: Todo) => (
         <div key={todo.id}>
           <h1>Id: {todo.id}</h1>
           <p>Title: {todo.title}</p>
           <p>Completed: {todo.completed ? "Yes" : "No"}</p>
         </div>
-      ))}
+      ))} */}
+      <form
+				onSubmit={(event) => {
+					event.preventDefault();
+					createPostMutation({
+						title,
+						body,
+						userId: USER_ID,
+					});
+				}}
+			>
+				<label htmlFor="title">Title</label>
+				<input
+					name="title"
+					id="title"
+					value={title}
+					onChange={(event) => {
+						setTitle(event.target.value);
+					}}
+				/>
+				<br />
+				<label htmlFor="body">Body</label>
+				<input
+					name="body"
+					id="body"
+					value={body}
+					onChange={(event) => {
+						setBody(event.target.value);
+					}}
+				/>
+				<button>Create Post</button>
+			</form>
     </div>
   );
 }
---
+++ src/utils/types.ts
@@ -0,0 +1,8 @@
+export interface UserResponseHttpData{
+
+id:number;
+name:string;
+username:string;
+email:string;
+
+}
